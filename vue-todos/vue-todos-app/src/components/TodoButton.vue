77fed2bba9fa72721a5518b8eded60d6d68953a2
azhar_azad <azhar.u.azad@gmail.com>
2023-07-27 21:14:41 +0600

    vue-todos-app phase 1 done

---
+++ vue-todos/vue-todos-app/src/components/TodoButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <button>
+        <!-- 'Create' is the default value if no value is passed from this component's caller -->
+        <slot>Create</slot> 
+    </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  padding: 8px 16px;
+  border: none;
+}
+</style>
