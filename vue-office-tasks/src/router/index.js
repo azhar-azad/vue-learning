666a071824a8dae6b1330233390c043654ea851e
azhar_azad <azhar.u.azad@gmail.com>
2023-08-03 13:09:22 +0600

    AppHeader component done

--- vue-office-tasks/src/router/index.js
+++ vue-office-tasks/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import TaskListView from '../views/TaskListView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'taskList',
+      component: TaskListView
     },
     {
       path: '/about',
