ac5a416d618c1473e9b1a68df54f21c3d09ba4ca
azhar_azad <azhar.u.azad@gmail.com>
2023-08-03 13:44:16 +0600

    TaskListCreator component done

--- vue-office-tasks/src/router/index.js
+++ vue-office-tasks/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'taskList',
+      name: 'taskLists',
       component: TaskListView
     },
     {
--- vue-office-tasks/src/views/TaskListView.vue
+++ vue-office-tasks/src/views/TaskListView.vue
@@ -1,8 +1,43 @@
 <script setup>
+import TaskListCreator from '../components/TaskList/TaskListCreator.vue';
+
 </script>
 
 <template>
   <main>
-    
+    <h1>Create Task List</h1>
+    <TaskListCreator />
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .task-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .task-list-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
